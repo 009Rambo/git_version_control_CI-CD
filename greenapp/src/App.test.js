ebaacad7aeb8776f2d3ea630555126efdf901b38
Ramesh Pandey <ramesh.pandey@tuni.fi>
2023-11-13 02:57:45 +0200

    some test files added and fixed

--- greenapp/src/App.test.js
+++ greenapp/src/App.test.js
@@ -1,8 +1,16 @@
+// App.test.js
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders current time without errors', () => {
+  // Render the component
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  // Check if the component renders without errors
+  expect(screen.getByText(/Local Time and To-Do List/i)).toBeInTheDocument();
+
+  // Check if the current time is displayed
+  expect(screen.getByText(/Current Time:/i)).toBeInTheDocument();
 });
+
+// You can add more specific tests for the clock if needed
